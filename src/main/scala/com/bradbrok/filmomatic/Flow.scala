cb0cf058c26bdc7128a837d2c9a4713ced91a160
Shea Rial <sheatrevor@gmail.com>
2017-03-14 00:19:12 -0700

    Expanded flow from an Enumeration into a full-fledged type so that it could have a ~> method which returned the valid possible subsequent values.

--- src/main/scala/com/bradbrok/filmomatic/Flow.scala
+++ src/main/scala/com/bradbrok/filmomatic/Flow.scala
@@ -1,6 +1,25 @@
 package com.bradbrok.filmomatic
 
-object Flow extends Enumeration {
-  type Flow = Value
-  val Idle, Fill, Settle, Agitate, Drain = Value
+object Flow {
+  sealed trait Flow {
+    /**
+      * @return valid possible subsequent values
+      */
+    def ~>(): Set[Flow]
+  }
+  case object Idle extends Flow {
+    val ~> = Set(Idle)
+  }
+  case object Fill extends Flow {
+    val ~> = Set(Settle, Agitate, Drain)
+  }
+  case object Settle extends Flow {
+    val ~> = Set(Agitate, Drain)
+  }
+  case object Agitate extends Flow {
+    val ~> = Set(Settle, Drain)
+  }
+  case object Drain extends Flow {
+    val ~> = Set(Idle, Fill)
+  }
 }
