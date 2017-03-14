9fb9307fe7bf33518885f8382cd171c432f05a03
Shea Rial <sheatrevor@gmail.com>
2017-03-14 00:34:28 -0700

    Renamed original "~>" method to "canBecome", and turned "~>" into a validator method.

--- src/main/scala/com/bradbrok/filmomatic/Flow.scala
+++ src/main/scala/com/bradbrok/filmomatic/Flow.scala
@@ -5,21 +5,31 @@ object Flow {
     /**
       * @return valid possible subsequent values
       */
-    def ~>(): Set[Flow]
+    def canBecome: Set[Flow]
+
+    /**
+      * @example
+      * {{{
+      * assert(Idle ~> Fill)
+      * }}}
+      * @param flow the subsequent value
+      * @return true if the subsequent is allowable
+      */
+    def ~>(flow: Flow): Boolean = canBecome contains flow
   }
   case object Idle extends Flow {
-    val ~> = Set(Idle)
+    val canBecome = Set(Idle, Fill)
   }
   case object Fill extends Flow {
-    val ~> = Set(Settle, Agitate, Drain)
+    val canBecome = Set(Settle, Agitate, Drain)
   }
   case object Settle extends Flow {
-    val ~> = Set(Agitate, Drain)
+    val canBecome = Set(Agitate, Drain)
   }
   case object Agitate extends Flow {
-    val ~> = Set(Settle, Drain)
+    val canBecome = Set(Settle, Drain)
   }
   case object Drain extends Flow {
-    val ~> = Set(Idle, Fill)
+    val canBecome = Set(Idle, Fill)
   }
 }
