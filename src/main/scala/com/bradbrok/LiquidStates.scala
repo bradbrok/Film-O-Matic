d460873ef21297a67fe91a51658c6fe6751e1476
Brad Brok <bradbrok@gmail.com>
2017-03-13 23:33:56 -0700

    Added more potential states

--- src/main/scala/com/bradbrok/LiquidStates.scala
+++ src/main/scala/com/bradbrok/LiquidStates.scala
@@ -5,5 +5,8 @@
 object LiquidStates extends Enumeration {
   type LiquidStates = Value
   //The bath states are A, B, & C. "F" means fill, "R" means reverse into same tank, "A" means Agitate
-  val BathAF, BathAR, BathAA, BathBF, BathBR, BathBA, BathCF, BathCR, BathCA, BathWater, BathWaste = Value
+  val BathAFill, BathAReverse, BathAAgitate, BathAWaste,
+    BathBFill, BathBReverse, BathBAgitate, BathBWaste,
+    BathCFill, BathCReverse, BathCAgitate, BathCWaste,
+    BathWaterFill, BathWaterWaste = Value
 }
