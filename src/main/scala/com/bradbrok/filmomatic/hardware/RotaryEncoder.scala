48a8f3dfe03c98ccb4b83792e01cdcfa4f57182e
Brad Brok <bradbrok@gmail.com>
2017-03-17 14:20:12 -0700

    Added ability to track rotary encoder steps

--- src/main/scala/com/bradbrok/filmomatic/hardware/RotaryEncoder.scala
+++ src/main/scala/com/bradbrok/filmomatic/hardware/RotaryEncoder.scala
@@ -4,20 +4,42 @@ package com.bradbrok.filmomatic.hardware
   * @author bradbrok on 3/16/17.
   */
 
-case class RotaryEncoder(pinA: Int, pinB: Int) {
+class RotaryEncoder(pinA: Int, pinB: Int) {
+  var rotationDelta: Int
+  var currentRotationValue: Int
+  var previousRotationValue: Int
+  var steps: Int = 0
 
   /** Decode the current state and return Int value.
     *
     * @param pinA is either 1 or 0
     * @param pinB is either 1 or 0
     * @return bitwsie conversion of input to 0..3
+    * 00 = 0
+    * 01 = 1
+    * 10 = 2
+    * 11 = 3
     */
   def decodeCurrentState = (pinA << 1) | pinB
+
+  def initRotationState = previousRotationValue = decodeCurrentState
+
+  //Needs a callback for when either value changes. A|B
+  def rotationDirectionUpdate(callback: Any)= {
+    currentRotationValue = decodeCurrentState
+    rotationDelta = 0
+    if(currentRotationValue != previousRotationValue){
+      rotationDelta = (previousRotationValue - currentRotationValue) % 4
+      if(rotationDelta == 3){
+        steps += -1
+      }
+      else{
+        steps += 1
+      }
+    }
+  }
 }
 
-object RotaryDirection {
-
-}
 
 
 
