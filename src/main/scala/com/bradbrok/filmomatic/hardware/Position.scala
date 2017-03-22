8b562da216fb7b86033064b3823e9890e7858b4b
Brad Brok <bradbrok@gmail.com>
2017-03-22 16:07:41 -0700

    More Position trait constructors to determine rotation

--- src/main/scala/com/bradbrok/filmomatic/hardware/Position.scala
+++ src/main/scala/com/bradbrok/filmomatic/hardware/Position.scala
@@ -4,16 +4,18 @@ package com.bradbrok.filmomatic.hardware
   * @author bradbrok on 3/17/17.
   */
 object Position {
-
-  sealed trait Position{
-    def apply(input: Int): Position = input match {
-      case 0 => A
-      case 1 => B
-      case 2 => D
-      case 3 => C
-    }
+  def apply(input: Int): Position = input match {
+    case 0 => A
+    case 1 => B
+    case 2 => D
+    case 3 => C
   }
-
+  sealed trait Position {
+    def leftOf: Position
+    def rightOf: Position
+    def <(position: Position): Boolean = position == leftOf
+    def >(position: Position): Boolean = position == rightOf
+  }
   case object A extends Position {
     val input = 0
     val leftOf = D
